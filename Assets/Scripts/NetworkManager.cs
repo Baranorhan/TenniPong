9ab286cb63d36575811422c7fca387663bda3196
Orhan Baran Cetin <baranorhan3@gmail.com>
2020-03-28 23:43:22 +0300

    Photon
    Photon added to project and created ew scene

---
+++ Assets/Scripts/NetworkManager.cs
@@ -0,0 +1,26 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NetworkManager : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+   private  void Start()
+    {
+        Debug.Log("connecting");
+
+        PhotonNetwork.GameVersion = "0.0.1";
+        PhotonNetwork.ConnectUsingSettings();
+
+    }
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("connected");
+    }
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        Debug.Log("disconected");
+    }
+}
